9b5424c1622276da8de9d7e53c503b395686fb1a
ssebastianoo <sebastianogirotto05@gmail.com>
2025-06-20 01:53:17 +0200

    commands

--- main.ts
+++ main.ts
@@ -10,7 +10,28 @@ const sql = postgres(process.env.DATABASE_URL);
 const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
 bot.onText(/\/add (.+)/, async (msg, match) => {
+    const args = match![1].split(' ').filter(x => x.length > 0);
+    console.log(msg.reply_to_message?.from);
+    // let user_id: number;
+    // let ability: string;
 
+
+    // if (msg.reply_to_message) {
+    //     user_id = msg.reply_to_message.from!.id;
+    //     ability = args[0];
+    // } else {
+
+    //     ability = args[1];
+    // }
+
+    // if (user_id === msg.from!.id) {
+    //     await bot.sendMessage(msg.chat.id, 'You cannot add yourself', { reply_to_message_id: msg.message_id });
+    //     return;
+    // }
+
+});
+
+bot.onText(/\/create (.+)/, async (msg, match) => {
     if (msg.chat.type != 'group') {
         bot.sendMessage(msg.chat.id, 'You are not in a group!');
         return
@@ -26,8 +47,29 @@ bot.onText(/\/add (.+)/, async (msg, match) => {
 
     if (match) {
         const ability = match[1];
-        await sql`INSERT INTO abilities (group_id, name) VALUES (${msg.chat.id}, ${ability})`;
-        bot.sendMessage(msg.chat.id, `Added ability ${ability}`);
+        try {
+            await sql`INSERT INTO abilities (group_id, name) VALUES (${msg.chat.id}, ${ability})`;
+        } catch (e) {
+            if (e.code === '23505')
+                await bot.sendMessage(msg.chat.id, `Ability ${ability} already exists`, { reply_to_message_id: msg.message_id });
+            else
+                await bot.sendMessage(msg.chat.id, 'Something went wrong', { reply_to_message_id: msg.message_id });
+            return;
+        }
+        await bot.sendMessage(msg.chat.id, `Added ability <b>${ability}</b>`, {
+            reply_to_message_id: msg.message_id,
+            parse_mode: 'HTML'
+        });
     } else
         bot.sendMessage(msg.chat.id, 'Please specify an ability');
+});
+
+bot.onText(/\/list/, async (msg) => {
+    const abilities = await sql`SELECT name FROM abilities WHERE group_id = ${msg.chat.id}`;
+    if (abilities.length == 0)
+        await bot.sendMessage(msg.chat.id, 'No abilities found', { reply_to_message_id: msg.message_id });
+    else {
+        const list = abilities.map(ability => `<b>${ability.name}</b>`).join('\n');
+        await bot.sendMessage(msg.chat.id, list, { reply_to_message_id: msg.message_id, parse_mode: 'HTML' });
+    }
 });
