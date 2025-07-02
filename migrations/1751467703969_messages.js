d99e1890d8f5cc779e459156197175fd453dfb98
chickenmatty <44543605+chickenmatty@users.noreply.github.com>
2025-07-02 16:50:28 +0200

    messages

---
+++ migrations/1751467703969_messages.js
@@ -0,0 +1,31 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.createTable('messages', {
+        message_id: { type: 'text', notNull: true },
+        chat_id: { type: 'text', notNull: true },
+        users: { type: 'jsonb', notNull: true },
+    }, {
+        primaryKey: ['message_id', 'chat_id'],
+    });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.dropTable('messages', {
+        ifExists: true,
+        cascade: true,
+    });
+};
